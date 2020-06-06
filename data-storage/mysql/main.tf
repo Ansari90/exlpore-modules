fbabdefb12292fb49e1bb99b4717b41768a1a790
Abdullah Akram Ansari <abdullaha90@gmail.com>
2020-06-06 19:21:40 -0400

    Setting skip_final_snapshot to true in mysql

--- data-storage/mysql/main.tf
+++ data-storage/mysql/main.tf
@@ -5,6 +5,7 @@ resource "aws_db_instance" "exploreDB" {
   instance_class = var.instance_type
   name = "${var.cluster_name}ExploreDB"
   username = "admin"
+  skip_final_snapshot = true
 
   password = var.db_password
 }
